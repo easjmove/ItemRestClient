9f0e81ce59428f679a858e4b0f99ea0875451835
move <move@zealand.dk>
2021-09-30 11:38:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercise1.models;
+using System;
+using System.Collections.Generic;
+
+namespace ItemRestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Worker myWorker = new Worker();
+
+            Item newItem = new Item(1, "Fodbold", 3, 7);
+            Item resultItem = myWorker.PostItem(newItem).Result;
+            Console.WriteLine($"{resultItem.Id} - {resultItem.Name} - {resultItem.ItemQuality} - {resultItem.Quantity}");
+            Console.WriteLine();
+            IEnumerable<Item> items = myWorker.GetAllItemsAsync().Result;
+            foreach(Item item in items)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} - {item.ItemQuality} - {item.Quantity}");
+            }
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise1.models;
+
+namespace ItemRestClient
+{
+   public class Worker
+    {
+        public async Task<IEnumerable<Item>> GetAllItemsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://exercise5.azurewebsites.net/api/items");
+                IEnumerable<Item> items = await response.Content.ReadFromJsonAsync<IEnumerable<Item>>();
+                return items;
+            }
+        }
+
+        public async Task<Item> PostItem(Item item)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                JsonContent serializedItem = JsonContent.Create(item);
+                HttpResponseMessage response = await client.PostAsync("https://exercise5.azurewebsites.net/api/items", serializedItem);
+                return await response.Content.ReadFromJsonAsync<Item>();
+            }
+        }
+    }
+}
